a1ab2fa3f512fe6db45d210e1594105270ecce37
wujingquan <i@wujingquan.com>
2021-03-11 17:12:23 +0800

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongoHighlighter } from '@mikro-orm/mongo-highlighter';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const clientUrl = `mongodb://${config.get('DB_USERNAME')}:${config.get(
+          'DB_PASSWORD',
+        )}@${config.get('DB_HOST')}:${config.get('DB_PORT')}/${config.get(
+          'DB_NAME',
+        )}`;
+        console.log(clientUrl);
+        return {
+          strict: true,
+          type: 'mongo',
+          clientUrl,
+          entities: ['./dist/entities'],
+          entitiesTs: ['./src/entities'],
+          debug: Boolean(config.get('DB_DEBUG')),
+          highlighter: new MongoHighlighter(),
+        };
+      },
+    }),
+    MikroOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/BaseEntity.ts
@@ -0,0 +1,19 @@
+import { PrimaryKey, Property, SerializedPrimaryKey } from '@mikro-orm/core';
+import { ObjectId } from '@mikro-orm/mongodb';
+
+export class BaseEntity {
+  @PrimaryKey()
+  _id!: ObjectId;
+
+  @SerializedPrimaryKey()
+  id!: string;
+
+  @Property()
+  createdAt = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Property()
+  deletedAt: Date;
+}
